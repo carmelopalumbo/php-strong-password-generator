6c034cab72c0bdfd2277ee71c271d90dc2e6edc5
Carmelo Palumbo <carmelopalumbo@outlook.it>
2022-12-02 15:36:44 +0100

    add-vars

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+
+
+$pwlength = $_GET['pw_length'] ?? '';
+$charrepeat = $_GET['same_chars'] ?? '';
+$charincludes = $_GET['characters'] ?? '';
+
+var_dump($pwlength);
+var_dump($charrepeat);
+var_dump($charincludes);
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,52 +31,55 @@
     <h1 class="py-5">STRONG PASSWORD GENERATOR</h1>
     <div class="container cp-container">
         <div class="row d-flex flex-column justify-content-center">
-            <div class="col">
-                <form action="">
+            <form action="./index.php" method="GET">
+
+                <div class="col">
+
                     <label for="basic-url" class="form-label">Inserisci lunghezza caratteri della password generata.</label>
                     <div class="input-group mb-3 w-50 m-auto">
-                        <input type="text" class="form-control text-center" id="basic-url" aria-describedby="basic-addon3">
+                        <input type="number" class="form-control text-center" id="basic-url" aria-describedby="basic-addon3" name="pw_length">
                     </div>
-                </form>
-            </div>
-
-            <div class="col d-flex justify-content-center">
-                <div class="form-check">
-                    <input class="form-check-input" type="radio" name="same_chars" id="flexRadioDefault1" checked>
-                    <label class="form-check-label" for="flexRadioDefault1">
-                        Caratteri uguali ammessi
-                    </label>
-                </div>
-                <div class="form-check mx-4">
-                    <input class="form-check-input" type="radio" name="same_chars" id="flexRadioDefault2">
-                    <label class="form-check-label" for="flexRadioDefault2">
-                        NO caratteri uguali
-                    </label>
-                </div>
-            </div>
-
-            <div class="col d-flex my-3 flex-column">
-
-                <div class="checkbox-form d-flex">
-                    <input type="checkbox" id="numbers" checked>
-                    <label for="numbers" class="mx-3">Numeri</label>
                 </div>
 
-                <div class="checkbox-form d-flex">
-                    <input type="checkbox" id="letters" checked>
-                    <label for="numbers" class="mx-3">Lettere</label>
+                <div class="col d-flex justify-content-center">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="same_chars" id="flexRadioDefault1" value="0" checked>
+                        <label class="form-check-label" for="flexRadioDefault1">
+                            Caratteri uguali ammessi
+                        </label>
+                    </div>
+                    <div class="form-check mx-4">
+                        <input class="form-check-input" type="radio" name="same_chars" id="flexRadioDefault2" value="1">
+                        <label class="form-check-label" for="flexRadioDefault2">
+                            NO caratteri uguali
+                        </label>
+                    </div>
                 </div>
 
-                <div class="checkbox-form d-flex">
-                    <input type="checkbox" id="specialchars" checked>
-                    <label for="numbers" class="mx-3">Caratteri speciali</label>
+                <div class="col d-flex my-3 flex-column">
+                    <p>Includi:</p>
+                    <div class="checkbox-form d-flex">
+                        <input type="checkbox" id="numbers" value="numbers" name="characters[]" checked>
+                        <label for="numbers" class="mx-3">Numeri</label>
+                    </div>
+
+                    <div class="checkbox-form d-flex">
+                        <input type="checkbox" id="letters" value="letters" name="characters[]" checked>
+                        <label for="numbers" class="mx-3">Lettere</label>
+                    </div>
+
+                    <div class="checkbox-form d-flex">
+                        <input type="checkbox" id="specialchars" value="specialchars" name="characters[]" checked>
+                        <label for="numbers" class="mx-3">Caratteri speciali</label>
+                    </div>
+
                 </div>
 
-            </div>
+                <div class="col d-flex justify-content-center py-3">
+                    <button class="btn btn-info" type="submit">GENERA</button>
+                </div>
 
-            <div class="col d-flex justify-content-center py-3">
-                <button class="btn btn-info">GENERA</button>
-            </div>
+            </form>
         </div>
     </div>
     </div>
